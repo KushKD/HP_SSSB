8a047961c85bec7cd4348606ae6730abd5bac22f
kuush <kushkumardhawan@gmail.com>
2016-04-27 11:44:51 +0530

    Added Services Project for the Mobile Applications.

---
+++ app/src/main/res/drawable/HPSSSB_WEB_API/HPSSSB_WEB_API/HPSSSB_REST.svc.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HPSSSB_WEB_API
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "HPSSSB_REST" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select HPSSSB_REST.svc or HPSSSB_REST.svc.cs at the Solution Explorer and start debugging.
+    public class HPSSSB_REST : IHPSSSB_REST
+    {
+
+
+
+        #region Basic Testing Functions Rest Architecture
+
+        public string XMLData(string id)
+        {
+            return id;
+        }
+
+
+        public string JSONData(string id)
+        {
+            return id;
+        }
+
+        #endregion
+
+        #region get Instructions XML and JSON Link
+       
+
+        public string XML_getInstructions()
+        {
+            try
+            {
+                return "http://hpsssb.hp.gov.in/Uploads/HPSSSB%20Instructions.pdf";
+            }
+            catch (Exception e)
+            {
+                return e.GetBaseException().ToString().Trim();
+            }
+        }
+
+
+        public string JSON_getInstructions()
+        {
+            try
+            {
+                return "http://hpsssb.hp.gov.in/Uploads/HPSSSB%20Instructions.pdf";
+            }
+            catch (Exception e)
+            {
+                return e.GetBaseException().ToString().Trim();
+            }
+        }
+        #endregion
+
+
+
+    }
+}
---
+++ app/src/main/res/drawable/HPSSSB_WEB_API/HPSSSB_WEB_API/IHPSSSB_REST.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.Script.Services;
+
+namespace HPSSSB_WEB_API
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IHPSSSB_REST" in both code and config file together.
+    [ServiceContract]
+    public interface IHPSSSB_REST
+    {
+        #region Test Functions Restful Services JSON and XML
+
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Xml, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "getxml/{id}")]
+        string XMLData(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "getjson/{id}")]
+        string JSONData(string id);
+
+        #endregion
+
+        #region Get Instructions (PDF File Link) in XML and JSON
+        [OperationContract]
+        [WebInvoke(Method ="GET",  ResponseFormat = WebMessageFormat.Xml, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate ="getInstructions_XML")]
+        string XML_getInstructions();
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "getInstructions_JSON")]
+        string JSON_getInstructions();
+
+        #endregion
+    }
+}
