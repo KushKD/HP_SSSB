3182ee2f2e440d5c80a847ab417e9015e5c3a02d
kuush <kushkumardhawan@gmail.com>
2016-05-02 13:24:32 +0530

    Dashboard Screen Updated and Working with fromdate and todate . Working on Post Wise Dashboard and Form Received Dashboard.

--- app/src/main/java/hp/dit/hpsssb/aadhaar/com/hpsssb/Dashboard.java
+++ app/src/main/java/hp/dit/hpsssb/aadhaar/com/hpsssb/Dashboard.java
@@ -1,14 +1,116 @@
 package hp.dit.hpsssb.aadhaar.com.hpsssb;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.app.Activity;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Dashboard extends Activity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import hp.dit.hpsssb.aadhaar.com.presentation.BaseActivity;
+
+public class Dashboard extends BaseActivity implements View.OnClickListener {
+
+    Button postWise , formsReceived;
+    private DatePickerDialog fromDatePickerDialog;
+    private DatePickerDialog toDatePickerDialog;
+    private SimpleDateFormat dateFormatter;
+    private TextView ext_FromDate , ext_ToDate;
+    private String GetFromDate = null;
+    private String GetToDate = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        postWise = (Button)findViewById(R.id.postwise);
+        formsReceived = (Button)findViewById(R.id.formsreceived);
+
+        ext_FromDate = (TextView)findViewById(R.id.fromdate);
+        ext_FromDate.setInputType(InputType.TYPE_NULL);
+
+        ext_ToDate = (TextView)findViewById(R.id.todate);
+        ext_ToDate.setInputType(InputType.TYPE_NULL);
+
+        dateFormatter = new SimpleDateFormat("MM.dd.yyyy", Locale.US);
+        setDateTimeField();
+
+        postWise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                GetFromDate = ext_FromDate.getText().toString().trim();
+                GetToDate = ext_ToDate.getText().toString().trim();
+
+                if(GetFromDate.length()!=0 && GetToDate.length()!=0){
+                    Toast.makeText(getApplicationContext(),GetFromDate +"###"+GetToDate,Toast.LENGTH_LONG).show();
+                    //Send From Date and To Date to Other Activity.
+
+                }else{
+                    Toast.makeText(getApplicationContext(),"Please input valid dates",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+        formsReceived.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GetFromDate = ext_FromDate.getText().toString().trim();
+                GetToDate = ext_ToDate.getText().toString().trim();
+
+                if(GetFromDate.length()!=0 && GetToDate.length()!=0){
+                    Toast.makeText(getApplicationContext(),GetFromDate +"###"+GetToDate,Toast.LENGTH_LONG).show();
+                    //Send From Date and To Date to Other Activity.
+
+                }else{
+                    Toast.makeText(getApplicationContext(),"Please input valid dates",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+    }
+
+    private void setDateTimeField() {
+        ext_FromDate.setOnClickListener(this);
+        ext_ToDate.setOnClickListener(this);
+
+        Calendar newCalendar = Calendar.getInstance();
+        fromDatePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                ext_FromDate.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+        toDatePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                ext_ToDate.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+    }
+    @Override
+    public void onClick(View v) {
+        if(v == ext_FromDate) {
+            fromDatePickerDialog.show();
+        }else if(v == ext_ToDate) {
+            toDatePickerDialog.show();
+        }
     }
 
 }
---
+++ app/src/main/java/hp/dit/hpsssb/aadhaar/com/hpsssb/DashboardList_PostWise.java
@@ -0,0 +1,14 @@
+package hp.dit.hpsssb.aadhaar.com.hpsssb;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class DashboardList_PostWise extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_post_wise_list);
+    }
+
+}
