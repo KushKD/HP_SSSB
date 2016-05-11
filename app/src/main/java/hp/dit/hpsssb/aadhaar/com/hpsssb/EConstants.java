1569a46567513d6528ae53a37d9767709e06b4bb
kuush <kushkumardhawan@gmail.com>
2016-05-11 12:35:47 +0530

    SplashScreen Updated

--- app/src/main/java/hp/dit/hpsssb/aadhaar/com/hpsssb/EConstants.java
+++ app/src/main/java/hp/dit/hpsssb/aadhaar/com/hpsssb/EConstants.java
@@ -8,7 +8,66 @@ public class EConstants {
     /**
      * HPSSSB MAin Screen
      */
+    public static final String url_Generic = "" ;
+    public static final String function_Instructions = "" ;
+    public static final String function_Vacancies = "" ;
+    public static final String function_GetOTP = "";
+    public static final String function_VerifyOTP = "";
+    public static final String function_DashboardCReport = "";
+    public static final String function_Dashboard = "";
+    public static final String function_getAdmitCardPersonalDetails = "";
+    public static final String function_getAdmitCardAadhaar = "";
+    public static final String HTTP_Verb_Get = "";
+    public static final String HTTP_Verb_post = "";
+    public static final int Connection_TimeOut = 0;
+    public static final String UNICODE = "";
+    public static final String InstructionsResult = "";
+    public static final String Vacancies_Result = "";
+    public static final String OTP_Result = "";
+    public static final String Login_Result="";
+    public static final String DashboardCReport_Result="";
+    public static final String Dashboard_Result = "";
+    public static final String AdmitCardAadhaar_Result = "";
+    public static final String AdmitCardPersonalDetails_Result = "";
+    public static final String Delemeter = ".";
+    public static final String progress_Dialog_Message = "Please be patient,as the application is trying to connect to the Internet";
+    public static final String Error_NoIdea = "Something went wrong, while fetching the results. Please try again later.";
+    public static final String Error_NoNetwork = "Unable to connect to Internet. Please check your network connection and try again.";
+    public static final String Path_PDF = "";
+    public static final String Intent_Type = "application/photos";
+    public static final int Chuck_Size = 1024;
+    public static final String Error_NoPDF_Viewer = "No Application Available to View PDF";
+    public static final String Error_DownloadFile = "The downloaded file is not a valid format.";
+    public static final String Messages_Results = "No pending results in pipeline.";
+    public static final String Messages_Interview="Currently there is no Interview Scheduled.";
 
+    /**
+     * HPSSSB Vacancies Page
+     */
+    public static final String Messages_Vacancis="There are no current vacancies.";
+    public static final String PutExtra_Message_Vacancies = "";
+    public static final String PutExtra_Message_Vacancies_Date ="";
+    public static final String ApplyButton_AlertMessage ="You'll be redirected to the http://hpsssb.hp.gov.in. Do you want to continue? Press Proceed to continue and Quit to exit.";
+    public static final String WebSite_Link = "http://hpsssb.hp.gov.in/vacancies.aspx";
+
+    /**
+     * Login Screen
+     */
+    public static final String ErrorMobile = "Please enter a valid mobile number.";
+    public static final String Messages_OTP = "Please wait , an OTP will be sent to this mobile number";
+    public static final String Date_Format = "";
+    public static final String Error_ValidDates = " input valid dates";
+    public static final String  Put_From_Date="";
+    public static final String  Put_To_Date="";
+
+    /**
+     * Admit Card
+     */
+    public static final String Put_Aadhaar  ="";
+    public static final String Put_Name  ="";
+    public static final String Put_DOB  ="";
+    public static final String Put_ApplicationID  ="";
+    public static final String Error_Aadhaar = "Please enter either your valid Aadhaar number or complete personal Details.";
 
 
 }
